c8475d7bc0199a5ddd5332b5c6892cf792c82927
Skylar <sscott28@cnm.edu>
2026-03-22 14:00:16 -0600

    Change by Claude: Add MainFragment as start destination
    Created MainFragment with navigation graph, layout, and string resource. MainFragment is set as the start destination with no actions.
    
    Prompt: use your add navigation fragment skill to make a MainFragment fragment that is the start destination with no actions (nowhere to go, yet)

---
+++ app/src/main/java/edu/cnm/deepdive/fossilsweeper/controller/MainFragment.java
@@ -0,0 +1,37 @@
+package edu.cnm.deepdive.fossilsweeper.controller;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import dagger.hilt.android.AndroidEntryPoint;
+import edu.cnm.deepdive.fossilsweeper.databinding.FragmentMainBinding;
+
+@AndroidEntryPoint
+public class MainFragment extends Fragment {
+
+  private FragmentMainBinding binding;
+
+  @Nullable
+  @Override
+  public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+      @Nullable Bundle savedInstanceState) {
+    binding = FragmentMainBinding.inflate(inflater, container, false);
+    return binding.getRoot();
+  }
+
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    // TODO: Set up ViewModel and observers
+  }
+
+  @Override
+  public void onDestroyView() {
+    binding = null;
+    super.onDestroyView();
+  }
+}
